8fbeb324122773a1b07b54ac22054e4d8fef9464
Tintswalo <110996874+calcaventia@users.noreply.github.com>
2024-02-17 01:23:17 +0200

    client setup

---
+++ client/src/components/navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const isAuth = false;
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <div>
+          <NavLink to="/">
+            <span className="navbar-brand mb-0 h1">Home</span>
+          </NavLink>
+        </div>
+
+        {isAuth ? (
+          <div>
+            <NavLink to="/dashboard" className="mx-3">
+              <span>Dashboard</span>
+            </NavLink>
+          </div>
+        ) : (
+          <div>
+            <NavLink to="/login" className="mx-3">
+              <span>Login</span>
+            </NavLink>
+            <NavLink to="/register" className="mx-3">
+              <span>Register</span>
+            </NavLink>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

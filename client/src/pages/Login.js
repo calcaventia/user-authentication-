9bb88d7c1ede6c66162e0e4423209048a5451166
Tintswalo <110996874+calcaventia@users.noreply.github.com>
2024-02-23 17:07:58 +0200

    front end fix

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,11 @@
 import { React, useState } from "react";
-import Layout from "../components/Layout";
 import { onLogin } from "../api/auth";
 import { useDispatch } from "react-redux";
 import { authenticateUser } from "../redux/slices/authSlice";
+import logo from "../images/Logo.png";
+import imageUrl from "../images/speaker.png";
+import "../Styles.css";
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
   const [values, setValues] = useState({
@@ -36,79 +39,95 @@ const Login = () => {
     }
   };
   return (
-    <Layout>
-      <form onSubmit={(e) => onSubmit(e)} className="container mt-3">
-        <h1>Login</h1>
+    <div className="mt-5 register-container">
+      <div className="row">
+        <div className="col-sm-6 rounded-start-5" id="form-column">
+          <img src={logo} alt="logo" className="logo mt-4" />
+          <form onSubmit={(e) => onSubmit(e)} className="form-box mt-5">
+            <h1 className="text-center fs-2 sign-up">Log In</h1>
+            <p className="text-center create-account">Log in to get started.</p>
+            <div className="mb-3">
+              <input
+                onChange={(e) => onChange(e)}
+                type="text"
+                className="form-control placeholder-box"
+                id="name"
+                name="name"
+                value={values.name}
+                placeholder="Name"
+                required
+              />
+            </div>
 
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            onChange={(e) => onChange(e)}
-            type="text"
-            className="form-control"
-            id="name"
-            name="name"
-            value={values.name}
-            placeholder="Name"
-            required
+            <div className="mb-3">
+              <input
+                onChange={(e) => onChange(e)}
+                type="email"
+                className="form-control placeholder-box"
+                id="email"
+                name="email"
+                value={values.email}
+                placeholder="Email Address"
+                required
+              />
+            </div>
+
+            <div className="mb-3">
+              <input
+                onChange={(e) => onChange(e)}
+                type="password"
+                className="form-control placeholder-box"
+                id="password"
+                name="password"
+                value={values.password}
+                placeholder="password"
+                required
+              />
+            </div>
+
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="rememberMe"
+                name="rememberMe"
+                checked={values.rememberMe}
+                onChange={(e) => onCheckboxChange(e)}
+              />
+              <label
+                className="form-check-label rememberMe"
+                htmlFor="rememberMe"
+              >
+                Remember Me
+              </label>
+            </div>
+
+            <div style={{ color: "red", margin: "10px 0" }}>{error}</div>
+
+            <button
+              type="submit"
+              className="btn btn-dark w-100"
+              id="login-button"
+            >
+              Login
+            </button>
+            <p className="mt-1 text-center create-account mt-3">
+              Don't have an account?
+              <NavLink to="/register" className="mx-3 register-link">
+                <span>Register</span>
+              </NavLink>
+            </p>
+          </form>
+        </div>
+        <div className="col-sm-6">
+          <img
+            src={imageUrl}
+            alt="sound speaker"
+            className="rounded-end-5 speaker-image"
           />
         </div>
-
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email Address
-          </label>
-          <input
-            onChange={(e) => onChange(e)}
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            value={values.email}
-            placeholder="test@gmail.com"
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            onChange={(e) => onChange(e)}
-            type="password"
-            className="form-control"
-            id="password"
-            name="password"
-            value={values.password}
-            placeholder="password"
-            required
-          />
-        </div>
-
-        <div className="mb-3 form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="rememberMe"
-            name="rememberMe"
-            checked={values.rememberMe}
-            onChange={(e) => onCheckboxChange(e)}
-          />
-          <label className="form-check-label" htmlFor="rememberMe">
-            Remember Me
-          </label>
-        </div>
-
-        <div style={{ color: "red", margin: "10px 0" }}>{error}</div>
-
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </Layout>
+      </div>
+    </div>
   );
 };
 
